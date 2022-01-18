0523575397c2d7231fc5b9f90118b92f46408ad1
Kamil <kamil.karasiewicz11@gmail.com>
2022-01-18 17:06:40 +0100

    update 2021-01-18

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -4,25 +4,114 @@ class AddUser extends React.Component{
     constructor(props){
         super(props)
 
-        this.state={
-
+        this.state ={
+            newName:"",
+            email:"",
+            newPassword:"",
+            passwordRepeat:""
         }
     }
 
     render (){
-      console.log("siema add user")
         return(
-            <div className="Add-user">
-                <input
-                  type="text"
-                  placeholder='Add new user'
-                  className="input"
-                  value = {this.state.newValue}
-                  onChange={this.inputUpdater.bind(this)}
-                />
-                <button className="button" onClick={this.addNewUser.bind(this)}>Add user</button>
-            </div>
+        <div className={this.props.addUserVisible}>
+            <input
+            type="text"
+            placeholder='Enter name'
+            className="input"
+            value={this.state.newName}
+            onChange={event=>this.InputUpdate("newName",event.target.value)}
+            />
+            <input
+            type="email"
+            name="email"
+            placeholder='Email'
+            className="input"
+            value={this.state.email}
+            onChange={event=>this.InputUpdate("email",event.target.value)}
+            />
+            <input
+            type="password"
+            placeholder='Enter password'
+            className="input"
+            value={this.state.newPassword}
+            onChange={event=>this.InputUpdate("newPassword",event.target.value)}
+            />
+            <input
+            type="password"
+            placeholder='Repeat password'
+            className="input"
+            value={this.state.passwordRepeat}
+            onChange={event=>this.InputUpdate("passwordRepeat",event.target.value)}
+            />
+            <button className="button" onClick={this.AddUser.bind(this)}>Accept</button>
+        </div>
         )
+    }
+    InputUpdate(key,value){
+        this.setState({
+            [key]:value
+        })
+    }
+    AddUser(){
+        if(this.state.newName === "" || this.state.email === ""){
+            alert("Please enter the name and email!")
+        }
+        else if(!this.state.email.includes("@")){
+            alert("Adress email needs to have '@'")
+        }
+        else{
+            const isCorrect = this.PasswordCheck(this.state.newPassword, this.state.passwordRepeat)
+            if(isCorrect){
+                const newUser = this.createUser();
+                this.props.newUserCreated(newUser);
+                this.props.changeVisibility("new-user-on");
+
+                this.setState({
+                    newName:"",
+                    email:"",
+                    newPassword:"",
+                    passwordRepeat:"",
+                    isCorrect: false
+                });
+            }
+        }
+    }
+    PasswordCheck(password,passwordRepeat){
+        if(password === "" || password.length <= 7){
+            alert("Please enter password which has more then 8!")
+        }
+        else if(password !== passwordRepeat){
+            alert("Passwords don't match!!!")
+        }
+        else{
+            if(password.charAt(0) !== password.charAt(0).toUpperCase()){
+                alert("First letter needs to be capital")
+            }
+            else{
+                return true;
+            }
+        }
+    }
+    createUser(){
+        const newUser={
+            id: Math.random()+1,
+            name:this.state.newName,
+            password:this.state.newPassword,
+            email:this.state.email.toLocaleLowerCase(),
+            tasks:[],
+            created:this.GetData(),
+            modify:this.GetData(),
+            status:"offline"
+            }
+        return newUser;
+    }
+    GetData(){
+        const newData = new Date();
+        const actualYear = newData.getFullYear()
+        const actualMonth = (newData.getMonth()+1).toString().padStart(2,"0")
+        const actualDay = newData.getDate()
+        return actualYear+"-"+actualMonth+"-"+actualDay;
     }
 }
 export default AddUser;
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,4 +1,5 @@
 import React from "react";
+import AddUser from "./AddUser";
 
 class Menu extends React.Component{
     constructor(props){
@@ -7,10 +8,6 @@ class Menu extends React.Component{
         this.state={
             newUserVisi:"off",
             visible:1,
-            newName:"",
-            newPassword:"",
-            passwordRepeat:"",
-            email:"",
             userToVisible:0
         }
     }
@@ -23,38 +20,7 @@ class Menu extends React.Component{
                     <h1>To Do Aplication</h1>
                     <button className="button" onClick={this.ChangeVisibility.bind(this)}>Create new user</button>
                 </div>
-                <div className={this.state.newUserVisi}>
-                    <input
-                    type="text"
-                    placeholder='Enter name'
-                    className="input"
-                    value={this.state.newName}
-                    onChange={event=>this.InputUpdate("newName",event.target.value)}
-                    />
-                    <input
-                    type="email"
-                    name="email"
-                    placeholder='Email'
-                    className="input"
-                    value={this.state.email}
-                    onChange={event=>this.InputUpdate("email",event.target.value)}
-                    />
-                    <input
-                    type="password"
-                    placeholder='Enter password'
-                    className="input"
-                    value={this.state.newPassword}
-                    onChange={event=>this.InputUpdate("newPassword",event.target.value)}
-                    />
-                    <input
-                    type="password"
-                    placeholder='Repeat password'
-                    className="input"
-                    value={this.state.passwordRepeat}
-                    onChange={event=>this.InputUpdate("passwordRepeat",event.target.value)}
-                    />
-                    <button className="button" onClick={this.AddUser.bind(this)}>Accept</button>
-                </div>
+                <AddUser addUserVisible ={this.state.newUserVisi} newUserCreated={this.NewUserCreated.bind(this)} changeVisibility={this.ChangeVisibility.bind(this)}/>
                 <div className="menu-list">
                     <button className="button-menu" onClick={() => this.props.showUsers(true)}>User List</button>
                     <button className="button-menu">Calendar</button>
@@ -78,70 +44,8 @@ class Menu extends React.Component{
             })
         }
     }
-    InputUpdate(key,value){
-        this.setState({
-            [key]:value
-        })
-    }
-    AddUser(){
-        if(this.state.newName === "" || this.state.email === ""){
-            alert("Please enter the name and email!")
-        }
-        else if(!this.state.email.includes("@")){
-            alert("Adress email needs to have '@'")
-        }
-        else{
-            const isCorrect = this.PasswordCheck(this.state.newPassword, this.state.passwordRepeat)
-            if(isCorrect){
-                const newUser = this.createUser();
-                this.props.parentCallback(newUser);
-                this.ChangeVisibility("new-user-on");
-
-                this.setState({
-                    newName:"",
-                    email:"",
-                    newPassword:"",
-                    passwordRepeat:"",
-                    isCorrect: false
-                });
-            }
-        }
-    }
-    PasswordCheck(password,passwordRepeat){
-        if(password === "" || password.length <= 7){
-            alert("Please enter password which has more then 8!")
-        }
-        else if(password !== passwordRepeat){
-            alert("Passwords don't match!!!")
-        }
-        else{
-            if(password.charAt(0) !== password.charAt(0).toUpperCase()){
-                alert("First letter needs to be capital")
-            }
-            else{
-                return true;
-            }
-        }
-    }
-    createUser(){
-        const newUser={
-            id: Math.random()+1,
-            name:this.state.newName,
-            password:this.state.newPassword,
-            email:this.state.email.toLocaleLowerCase(),
-            tasks:[],
-            created:this.GetData(),
-            modify:this.GetData(),
-            status:"offline"
-            }
-        return newUser;
-    }
-    GetData(){
-        const newData = new Date();
-        const actualYear = newData.getFullYear()
-        const actualMonth = (newData.getMonth()+1).toString().padStart(2,"0")
-        const actualDay = newData.getDate()
-        return actualYear+"-"+actualMonth+"-"+actualDay;
+    NewUserCreated(newUser){
+        this.props.parentCallback(newUser)
     }
 }
 export default Menu;
